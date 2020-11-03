Replace fixed interval image scaling with zscale interval image scaling.

neossat/improcess.py
@@ -9,6 +9,7 @@ from scipy import fftpack
 
 from astropy.io import fits
 from astropy.stats import sigma_clipped_stats
+from astropy.visualization import ZScaleInterval
 from photutils import DAOStarFinder, CircularAperture, aperture_photometry
 
 from . import utils
@@ -222,6 +223,29 @@ def scale_image(image, ref_image, mind=0, maxd=8000, b1=100, m1=0.3, m2=1.3, tp=
     return scaled_image
 
 
+def scale_image_zscale(image, ref_image, b1=100, m1=0.3, m2=1.3, tp=2000):
+    """"""
+
+    data = image.flatten()
+    ref_data = ref_image.flatten()
+
+    min1, max1 = ZScaleInterval().get_limits(data)
+    min2, max2 = ZScaleInterval().get_limits(ref_data)
+
+    mask = (data > min1) & (data < max1) & (ref_data > min2) & (ref_data < max2)
+
+    if np.sum(mask) > 10:
+        data_bin, ref_data_bin, err_bin = utils.bindata(data[mask], ref_data[mask], 50)
+
+        x0 = [b1, m1, m2, tp]
+        ans = optimize.least_squares(ls_seg_func, x0, args=[data_bin, ref_data_bin, err_bin])
+
+        scaled_data = seg_func(ans.x, data)
+        scaled_image = scaled_data.reshape(image.shape)
+
+    return scaled_image
+
+
 def combinedarks(alldarkdata, mind=0, maxd=8000, b1=100, m1=0.3, m2=1.3, tp=2000):
     """
     mind,maxd : range of data to consider when matching frames.  Keeping maxd relatively low avoids stars
@@ -239,7 +263,8 @@ def combinedarks(alldarkdata, mind=0, maxd=8000, b1=100, m1=0.3, m2=1.3, tp=2000
         image = alldarkdata[i]
         ref_image = alldarkdata[0]
 
-        newdark = scale_image(image, ref_image, mind=mind, maxd=maxd, b1=b1, m1=m1, m2=m2, tp=tp)
+        # newdark = scale_image(image, ref_image, mind=mind, maxd=maxd, b1=b1, m1=m1, m2=m2, tp=tp)
+        newdark = scale_image_zscale(image, ref_image, b1=b1, m1=m1, m2=m2, tp=tp)
         darkscaled.append(newdark)
 
     darkscaled = np.array(darkscaled)
@@ -585,7 +610,8 @@ def clean_sciimage(filename, darkavg, xsc, ysc, xov, yov, snrcut, fmax, xoff, yo
         # scidata_cord = scidata_cor-(a+b*darkavg)
 
         # New Dark-correction. Not extensively tested. No Fortran dependence.
-        newdark = scale_image(darkavg, scidata_cor)
+        # newdark = scale_image(darkavg, scidata_cor)
+        newdark = scale_image_zscale(darkavg, scidata_cor)
         scidata_cord = scidata_cor - newdark
 
     # Return if only clipping and overscan is performed.